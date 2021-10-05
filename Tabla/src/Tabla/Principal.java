e563351fc14145734d16ce8cb5a83e702cf98c02
Miguel Campos <miguel.campos@salesianos.edu>
2021-10-05 13:13:21 +0200

    feat: miguel ha terminado, jesús suspenso.

--- Tabla/src/Tabla/Principal.java
+++ Tabla/src/Tabla/Principal.java
@@ -1,14 +1,19 @@
 package Tabla;
 
-public class Principal {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		for (int i = 0; i<=10; i++) {
-			
+			// Jesús no vayas a tocar esta línea
 			for (int j = 0; j <=6; j++) {
 				System.out.println(i*j);
+				// Ni esta tampoco, que te conozco
+				for (int k = 0; k< 100; k++) {
+					System.out.println(k*2*i);
+					System.out.println("Terminado!!! Tienes un 0 Jesús");
+				}
 			}
 		}
 
