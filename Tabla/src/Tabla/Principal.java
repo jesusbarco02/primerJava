926d58527a9ff419b19760d46cfec8c7c72efbbe
BARCO <barco.esjes20@triana.salesianos.edu>
2021-10-05 13:00:04 +0200

    Se ha realizado la aplicación correctamente

--- Tabla/src/Tabla/Principal.java
+++ Tabla/src/Tabla/Principal.java
@@ -5,13 +5,14 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 0; i<=10; i++) {
+
 			
 			for (int j = 0; j <=6; j++) {
 				System.out.println(i*j);
+				System.out.println("Miguel ponme un 10 hombre que me lo merezco");
 			}
-		}
 
-	}
+
+
 
 }
